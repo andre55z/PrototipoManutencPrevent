8008b2dd6e7efbd1b91e44867c6c3b7b568480ff
andredev <andluccag@gmail.com>
2025-04-01 21:37:49 -0300

    Commit completo da pagina de login (01/04/24)

--- code.js
+++ code.js
@@ -1,5 +1,21 @@
 const openWidgetButton = document.getElementById('btncadastro');
 const closeWidgetButton = document.getElementById('closeWidget');
+const confirmMat = document.getElementById('confirmMat');
+const widget = document.getElementById('widget');
+const messageCont = document.getElementById('messageContainer');
+
+
+const matricula = document.getElementById('matricula');
+const setor = document.getElementById('setor');
+const senha = document.getElementById('pswd');
+const confirmarSenha = document.getElementById('confpswd');
+const entrar = document.getElementById('Entrar');
+const setorop = document.getElementById('setorLogin');
+const widchoose = document.getElementById('widgetcho');
+const widsp = document.getElementById('widsp');
+const closeWidgetchoButton = document.getElementById('closeWidgetcho');
+const paradaMaquina = document.getElementById('btparadademaq');
+const servicoProg = document.getElementById('btservicoprog');
 
 openWidgetButton.addEventListener('click', () => {
     widget.classList.remove('hidden');
@@ -8,4 +24,72 @@ openWidgetButton.addEventListener('click', () => {
 
 closeWidgetButton.addEventListener('click', () => {
     widget.classList.remove('visible');
-    widget.classList.add('hidden');})
+    widget.classList.add('hidden');
+});
+
+
+confirmMat.addEventListener('click', () => {
+    if (matricula.value.trim() === '' || setor.value.trim() === '' || senha.value.trim() === '' || confirmarSenha.value.trim() === '') {
+        alert('Por favor, preencha todos os campos!');
+        return;
+    }
+
+    if (senha.value !== confirmarSenha.value) {
+        alert('As senhas não coincidem!');
+        return;
+    }
+
+
+    widget.classList.remove('visible');
+    widget.classList.add('hidden');
+
+ 
+    messageCont.style.display = 'block';
+
+ 
+    setTimeout(() => {
+        messageCont.style.display = 'none';
+    }, 3000);
+});
+
+entrar.addEventListener('click', ()=>
+    {
+        const SetorSelected = setorop.value;
+        if (SetorSelected === "Engenharia/produção (Nível Técnico/Líder)")
+            {
+                window.location.href = './engprodtec.html';
+            }
+            else if (SetorSelected === "Engenharia/produção (Nível Engenharia)")
+            {
+                window.location.href = './engprodeng.html';
+            }
+            else if (SetorSelected==="Supervisor")
+            {
+                widgetcho.classList.remove('hidden');
+                widgetcho.classList.add('visible');
+                widsp.classList.remove('hidden');
+                widsp.classList.add('visible');
+
+                closeWidgetchoButton.addEventListener('click', () => {
+                    widsp.classList.remove('visible');
+                    widsp.classList.add('hidden');
+                    widgetcho.classList.remove('visible');
+                    widgetcho.classList.add('hidden');
+                });
+
+                paradaMaquina.addEventListener('click', ()=>
+                {
+                    window.location.href='./paradamaquina.html';
+                });
+                servicoProg.addEventListener('click', ()=>
+                {
+                    window.location.href='./servicoprog.html';
+                });
+
+
+            }
+            else
+            {
+                alert("Por favor, selecione um setor!")
+            }
+    });
