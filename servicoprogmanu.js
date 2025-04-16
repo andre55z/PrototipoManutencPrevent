333ba19416cbf41de70062982f4336af972860d2
andredev <andluccag@gmail.com>
2025-04-16 15:13:53 -0300

    Commit de atualização do detalhes danotificação(16/04)

--- servicoprogmanu.js
+++ servicoprogmanu.js
@@ -1,1 +1,8 @@
+function showWidget(item) {
+    document.getElementById("widgetText").textContent = item;
+    document.getElementById("widdesc").classList.add("visible");
+}
 
+function hideWidget() {
+    document.getElementById("widdesc").classList.remove("visible");
+}
