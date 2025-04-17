56d0bf6c3df1e4e09a7062fd9e9e6e52808a56f7
andredev <andluccag@gmail.com>
2025-04-17 11:09:37 -0300

    Commit final da jornada da manutenção no serviço prog

--- servicoprogmanu.js
+++ servicoprogmanu.js
@@ -3,6 +3,35 @@ function showWidget(item) {
     document.getElementById("widdesc").classList.add("visible");
 }
 
-function hideWidget() {
-    document.getElementById("widdesc").classList.remove("visible");
-}
+const chkstp = document.getElementById("chkstp");
+const chkconclu = document.getElementById("chkconclu");
+const closebt = document.getElementById("Fechar");
+const registrarbt = document.getElementById("Registrar");
+const widdesc = document.getElementById("widdesc");
+
+chkstp.addEventListener('change', ()=>
+    {
+        if (chkconclu.checked) chkstp.checked = false;
+    });
+chkconclu.addEventListener('change', ()=>
+    {
+        if (chkstp.checked) chkconclu.checked = false;
+    });
+
+closebt.addEventListener('click', ()=>{
+    widdesc.classList.remove("visible");
+});
+
+registrarbt.addEventListener('click', ()=>
+    {
+        const confirmado = confirm("Tem certeza que deseja registrar?");
+        if (!confirmado)
+        {
+            e.preventDefault();
+        }
+        else
+        {
+            alert("Notificação atualizada!");
+            widdesc.classList.remove("visible");
+        }
+    })
